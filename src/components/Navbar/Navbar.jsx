5b07c064f23bad8368906cf06ffe0bb93320c8a5
DADAKIM <d.ya96v@gmail.com>
2022-12-11 20:07:31 +0900

    :art: style : 내브바 스타일드 컴포넌트 분리 및 수정

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import imgHome from "../../assets/images/icon-home.png";
 import imgMessage from "../../assets/images/icon-message-circle.png";
 import imgEdit from "../../assets/images/icon-edit.png";
 import imgProfile from "../../assets/images/icon-user.png";
-import styled from "styled-components";
-
-const StyledLink = styled(Link)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 4px;
-  border: 1px solid salmon;
-  font-size: 10px;
-  padding: 12px 0 6px;
-  text-decoration: none;
-  color: #767676;
-  width: 84px;
-`;
-
-const StyledNav = styled.nav`
-	display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  padding: 0 6px;
-  width: 390px;
-  height: 60px;
-  margin: 0 auto;
-`;
-
-const StyledImg = styled.img`
-	width: 22px;
-`;
+import { StyledLink, StyledNav, StyledImg } from "./styledNavBar";
 
 export default function Navbar() {
   return (
