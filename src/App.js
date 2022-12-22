1a9193143f02c623db38474f7a5b6862ababfecf
jhfrontend <kwonjhjyj@naver.com>
2022-12-23 01:02:07 +0900

    :adhesive_bandage: fix: 충돌해결

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ import ProfileEdit from "./pages/Profile/ProfileEdit/ProfileEdit";
 import JoinWithEmail from "./components/Join/JoinWithEmail/JoinWithEmail";
 import { useEffect, useState } from "react";
 import Splash from "./components/Splash/Splash";
+import Product from "./pages/Product/Product";
+import ChatRoom from "./components/ChatRoom/ChatRoom";
+import Login from "./components/common/Login/Login";
 
 // 잠시 1200 -> 100으로 변경
 function App() {
@@ -26,6 +29,7 @@ function App() {
   });
 
   return (
+    <>
     <UserContextProvider>
       <Container>
         <GlobalStyle />
@@ -64,6 +68,47 @@ function App() {
         </Wrapper>
       </Container>
     </UserContextProvider>
+    <Container>
+      <GlobalStyle />
+      <Aside>
+        <img src="https://cdn.pixabay.com/photo/2017/10/05/09/37/equalizer-2818803_960_720.jpg" />
+      </Aside>
+      <Wrapper>
+        {isFirst ? (
+          <Splash />
+        ) : (
+          <BrowserRouter>
+            <Main>
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/profile" element={<Profile />} />
+                <Route path="/profile/edit" element={<ProfileEdit />} />
+                <Route path="/profile/:accountname" element={<ProfileEdit />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/chat/chatroom" element={<ChatRoom />} />
+                <Route path="/post" element={<Post />} />
+                <Route path="/post/postdetail" element={<PostDetail />} />
+                <Route path="/join" element={<JoinWithEmail />} />
+                <Route path="/login" element={<Login />} />
+                <Route
+                  path="/profile/:accountname/follower"
+                  element={<Follow />}
+                />
+                <Route
+                  path="/profile/:accountname/following"
+                  element={<Follow />}
+                />
+                <Route path="/product" element={<Product />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Main>
+            <Navbar />
+          </BrowserRouter>
+        )}
+      </Wrapper>
+    </Container>
+    </>
   );
 }
 export default App;
--- src/components/common/Login/Login.jsx
+++ src/components/common/Login/Login.jsx
@@ -1,37 +1,88 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import useLogin from "../../../hooks/useLogin";
+import { WarningMessageWrapper, LoginButton, LoginButtonWrapper} from "../../common/Login/Login.style"
+import { Container, InputTitle, Input } from "../../common/Login/Login.style"
+import { Link } from "react-router-dom";
 
 export default function Login() {
   const [emailInp, setEmailInp] = useState("");
   const [passInp, setPassInp] = useState("");
   const { error, isPending, login } = useLogin();
-
-  const handleInp = (event) => {
-    switch (event.target.id) {
-      case "userEmail":
-        setEmailInp(event.target.value);
-        break;
-      case "userPass":
-        setPassInp(event.target.value);
-        break;
-      default:
-        alert("뭔가 잘못됨.");
-        break;
-    }
-  };
+  const [emailErrorMsg, setEmailErrorMsg] = useState("");
+  const [passwordErrorMsg, setPasswordErrorMsg] = useState("");
+  const [isActive, setIsActive] = useState(true);
+  const [loginValid, setLoginValid] = useState(true);
+  const [passwordValid, setpasswordValid] = useState(true);
+  const userEmailRef = useRef("");
+  const userPasswordRef = useRef("");
 
   const handleLogin = (event) => {
     event.preventDefault();
 
     login(emailInp, passInp);
   };
+
+  //이메일 유효성 검사
+  const emailValidCheck = () => {
+    const userEmail = userEmailRef.current.value;
+    const emailCheck =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+      if(!emailCheck.test(userEmail)) {
+        setEmailErrorMsg('*잘못된 이메일 형식입니다.');
+        setLoginValid(false);
+        return;
+
+      }
+      setEmailErrorMsg('');
+      
+  }
+
+  const goToLogin = () => {
+    return loginValid && passwordValid ? setIsActive(false) : setIsActive(true);
+  };
+
+  const passwordValidCheck = () => {
+    const userPassword = userPasswordRef.current.value;
+
+    if(userPassword.length < 6) {
+      setPasswordErrorMsg('*비밀번호는 6자 이상이어야 합니다.');
+      return;
+    }
+    setPasswordErrorMsg('');
+    setLoginValid(true);
+  };
+
   return (
-    <form>
-      <label htmlFor="email">이메일</label>
-      <input id="userEmail" type="email" required onChange={handleInp} />
-      <label htmlFor="pass">비밀번호</label>
-      <input id="userPass" type="password" required onChange={handleInp} />
-      <button onClick={handleLogin}>제출</button>
-    </form>
+    <Container>
+      <h1 className="hidden">무지개마켓 로그인</h1>
+      <h2 className="login-title">로그인</h2>
+      <form className="login-form" onSubmit={handleLogin}>
+        <InputTitle>이메일</InputTitle>
+        <Input
+          validTest={loginValid}
+          type="email"
+          ref={userEmailRef}
+          onKeyUp={goToLogin}
+          onChange={emailValidCheck}
+          placeholder="이메일 주소를 입력해주세요"
+        />
+        <WarningMessageWrapper>{emailErrorMsg}</WarningMessageWrapper>
+        <InputTitle>비밀번호</InputTitle>
+        <Input
+          validTest={passwordValid}
+          type="password"
+          ref={userPasswordRef}
+          onKeyUp={goToLogin}
+          onChange={passwordValidCheck}
+          placeholder="비밀번호를 입력해 주세요"
+        />
+        <WarningMessageWrapper>{passwordErrorMsg}</WarningMessageWrapper>
+        <LoginButtonWrapper>
+          <LoginButton disabled={isActive}>로그인</LoginButton>
+          <Link to = '/join'>이메일로 회원가입</Link>
+        </LoginButtonWrapper>
+      </form>
+    </Container>
   );
 }
