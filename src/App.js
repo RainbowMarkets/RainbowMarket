59f7b27bff20113457bbd985f1c8557144ed859a
yjlee9909 <yjlee9909@naver.com>
2022-12-23 10:11:08 +0900

    :adhesive_bandage: fix : 충돌해결

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Follow from "./pages/Follow/Follow";
 import { UserContextProvider } from "./context/UserContext";
 import PostDetail from "./components/Posts/PostDetail";
 import ProfileEdit from "./pages/Profile/ProfileEdit";
+import MyProfile from "./pages/Profile/MyProfile";
 import JoinWithEmail from "./components/Join/JoinWithEmail/JoinWithEmail";
 import { useEffect, useState } from "react";
 import Splash from "./components/Splash/Splash";
@@ -43,12 +44,17 @@ function App() {
               <Main>
                 <Routes>
                   <Route exact path="/" element={<Home />} />
-                  <Route exact path="/profile" element={<Profile />} />
-                  <Route path="/profile/edit" element={<ProfileEdit />} />
+                  <Route exact path="/profile" element={<MyProfile />} />
                   <Route
+                    exact
+                    path="/profile/:accountname"
+                    element={<Profile />}
+                  />
+                  <Route path="/profile/edit" element={<ProfileEdit />} />
+                  {/* <Route
                     path="/profile/:accountname"
                     element={<ProfileEdit />}
-                  />
+                  /> */}
                   <Route path="/search" element={<Search />} />
                   <Route path="/chat" element={<Chat />} />
                   <Route path="/chat/chatroom" element={<ChatRoom />} />
