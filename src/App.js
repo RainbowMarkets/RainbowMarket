efaad90d3162a61e67a2e78f1d30f2ece3ecb10a
yjlee9909 <yjlee9909@naver.com>
2022-12-23 10:03:55 +0900

    :adhesive_bandage: fix : 충돌해결

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Chat from "./pages/Chat/Chat";
 import Follow from "./pages/Follow/Follow";
 import { UserContextProvider } from "./context/UserContext";
 import PostDetail from "./components/Posts/PostDetail";
-import ProfileEdit from "./pages/Profile/ProfileEdit/ProfileEdit";
+import ProfileEdit from "./pages/Profile/ProfileEdit";
 import JoinWithEmail from "./components/Join/JoinWithEmail/JoinWithEmail";
 import { useEffect, useState } from "react";
 import Splash from "./components/Splash/Splash";
@@ -29,46 +29,51 @@ function App() {
   });
 
   return (
-    <Container>
-      <GlobalStyle />
-      <Aside>
-        <img src="https://cdn.pixabay.com/photo/2017/10/05/09/37/equalizer-2818803_960_720.jpg" />
-      </Aside>
-      <Wrapper>
-        {isFirst ? (
-          <Splash />
-        ) : (
-          <BrowserRouter>
-            <Main>
-              <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/profile" element={<Profile />} />
-                <Route path="/profile/edit" element={<ProfileEdit />} />
-                <Route path="/profile/:accountname" element={<ProfileEdit />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/chat/chatroom" element={<ChatRoom />} />
-                <Route path="/post" element={<Post />} />
-                <Route path="/post/postdetail" element={<PostDetail />} />
-                <Route path="/join" element={<JoinWithEmail />} />
-                <Route path="/login" element={<Login />} />
-                <Route
-                  path="/profile/:accountname/follower"
-                  element={<Follow />}
-                />
-                <Route
-                  path="/profile/:accountname/following"
-                  element={<Follow />}
-                />
-                <Route path="/product" element={<Product />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Main>
-            <Navbar />
-          </BrowserRouter>
-        )}
-      </Wrapper>
-    </Container>
+    <UserContextProvider>
+      <Container>
+        <GlobalStyle />
+        <Aside>
+          <img src="https://cdn.pixabay.com/photo/2017/10/05/09/37/equalizer-2818803_960_720.jpg" />
+        </Aside>
+        <Wrapper>
+          {isFirst ? (
+            <Splash />
+          ) : (
+            <BrowserRouter>
+              <Main>
+                <Routes>
+                  <Route exact path="/" element={<Home />} />
+                  <Route exact path="/profile" element={<Profile />} />
+                  <Route path="/profile/edit" element={<ProfileEdit />} />
+                  <Route
+                    path="/profile/:accountname"
+                    element={<ProfileEdit />}
+                  />
+                  <Route path="/search" element={<Search />} />
+                  <Route path="/chat" element={<Chat />} />
+                  <Route path="/chat/chatroom" element={<ChatRoom />} />
+                  <Route path="/post" element={<Post />} />
+                  <Route path="/post/postdetail" element={<PostDetail />} />
+                  <Route path="/join" element={<JoinWithEmail />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route
+                    path="/profile/:accountname/follower"
+                    element={<Follow />}
+                  />
+                  <Route
+                    path="/profile/:accountname/following"
+                    element={<Follow />}
+                  />
+                  <Route path="/product" element={<Product />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </Main>
+              <Navbar />
+            </BrowserRouter>
+          )}
+        </Wrapper>
+      </Container>
+    </UserContextProvider>
   );
 }
 export default App;
