b84c569c80d1ad47741af945873d7b89bc914156
yjlee9909 <yjlee9909@naver.com>
2022-12-26 10:30:32 +0900

    :construction_worker: feat : 게시글 업로드 이미지 url 변환하여 미리보기

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -18,13 +18,31 @@ const Post = (props) => {
   const textRef = useRef();
   const [profileImg, setProfileImg] = useState("");
   const [inpValue, setInpValue] = useState("");
-  const [imgFile, setImgFile] = useState("");
-  const [imgFileURL, setImgFileURL] = useState("");
+  const [fileName, setFileName] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+  const [previewImgUrl, setPreviewImgUrl] = useState([]);
+  const imgRef = useRef();
   const { user } = useUserContext();
   const url = "https://mandarin.api.weniv.co.kr";
   const localAccountName = localStorage.getItem("aName");
-  console.log(localAccountName);
-  // 개인 프로필 가져오기
+
+  // textarea
+  const handleResizeHeight = () => {
+    textRef.current.style.height = "auto";
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  };
+
+  const handleInpVal = (e) => {
+    setInpValue(e.target.value);
+    console.log(inpValue);
+    // if (e.target.value) {
+    //   setIsActive(true);
+    // } else if (!e.target.value && fileName.length === 0) {
+    //   setIsActive(false);
+    // }
+  };
+
+  // 작성자 프로필 가져오기
   const fetchProfile = async () => {
     const reqPath = `/profile/${localAccountName}`;
     try {
@@ -43,6 +61,47 @@ const Post = (props) => {
       console.log("err", err);
     }
   };
+  // 이미지 서버에 전송하기
+  const fetchImgServer = async (file) => {
+    const reqPath = `/image/uploadfiles`;
+    const loadImg = file.target.files;
+    const formData = new FormData();
+    formData.append("image", loadImg[0]);
+    try {
+      const res = await fetch(url + reqPath, {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) =>
+          setFileName([...fileName, url + "/" + data[0].filename])
+        );
+      // .then((data) => console.log(url + "/" + data));
+      // 이미지 미리 보여주기
+      previewImg(loadImg);
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+  // 이미지 미리 보여주기
+  const previewImg = (loadImg) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(loadImg[0]);
+    // console.log(loadImg[0]);
+
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        // const previewImgUrl = reader.result;
+        // console.log(previewImgUrl);
+        setPreviewImgUrl([...previewImgUrl, reader.result]);
+        resolve();
+        console.log(previewImgUrl);
+      };
+    });
+  };
+
+  // 이미지 삭제하기
+  const handleDeleteImg = (e) => {};
 
   // 게시글 작성하기
   const fetchPostHandler = async () => {
@@ -64,24 +123,11 @@ const Post = (props) => {
         }),
       })
         .then((res) => res.json())
-        // .then((data) => {
-        //   // setPostDetailData(data.post);
-        //   // setIsHeartOn(data.post.hearted);
-        //   // setLikeCount(data.post.heartCount);
-        // })
+
         .then(() => window.location.assign("/profile"));
     } catch (err) {
       console.log("err", err);
     }
-  };
-  const handleResizeHeight = () => {
-    textRef.current.style.height = "auto";
-    textRef.current.style.height = textRef.current.scrollHeight + "px";
-  };
-
-  const handleInpVal = (e) => {
-    setInpValue(e.target.value);
-    console.log(inpValue);
   };
 
   useEffect(() => {
@@ -89,25 +135,8 @@ const Post = (props) => {
     fetchProfile();
   }, []);
 
-  // 파일 미리보기 로직
-  // const handleLoadFile = (e) => {
-  //   const reader = new FileReader();
-  //   reader.readAsDataURL(e.target.files[0]);
-  //   return new Promise((resolve) => {
-  //     reader.onload = () => {
-  //       setImgFile(reader.result);
-  //       resolve();
-  //       console.log(imgFile);
-  //     };
-  //   });
-  // };
-
-  // 이미지 미리보기 삭제하기
-  const handleDeleteImg = (e) => {
-    setImgFile("");
-  };
-
-  // 1. 프로필 이미지 가져오기
+  console.log(fileName);
+  // 1. 프로필 이미지 가져오기 (V)
   // 2. 파일 선택시 화면에 띄우기 (V) -> 일단 먼저 글만 보내는 거 구현하기
   // 3. 업로드 버튼 누르면 이미지 새로 고침
   return (
@@ -116,6 +145,7 @@ const Post = (props) => {
         setInpValue={setInpValue}
         inpValue={inpValue}
         handler={fetchPostHandler}
+        fetchImgServer={fetchImgServer}
       />
       <UploadContain>
         <UploadWrapper>
@@ -141,17 +171,26 @@ const Post = (props) => {
                 <PostImgWrapper>
                   <h4 className="hidden">이미지추가</h4>
                   <ul>
-                    {imgFile && (
-                      <li>
-                        <img src={imgFile} alt="이미지" />
-                        <button
-                          className="postImg-del"
-                          onClick={handleDeleteImg}
-                        >
-                          <span className="hidden">이미지 삭제</span>
-                        </button>
-                      </li>
-                    )}
+                    {previewImgUrl &&
+                      previewImgUrl.map((item, idx) => {
+                        return (
+                          <li key={idx}>
+                            <img src={item} alt="이미지" />
+                            <button
+                              className="postImg-del"
+                              onClick={handleDeleteImg}
+                            >
+                              <span className="hidden">이미지 삭제</span>
+                            </button>
+                          </li>
+                        );
+                      })}
+                    {/* <li>
+                      <img src={previewImgUrl} alt="이미지" />
+                      <button className="postImg-del">
+                        <span className="hidden">이미지 삭제</span>
+                      </button>
+                    </li> */}
                   </ul>
                 </PostImgWrapper>
               </div>
@@ -166,6 +205,8 @@ const Post = (props) => {
                   accept="image/*"
                   className="hidden"
                   /*onChange={handleLoadFile}*/
+                  onChange={fetchImgServer}
+                  ref={imgRef}
                 />
               </div>
             </form>
