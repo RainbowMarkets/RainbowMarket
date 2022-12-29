9c31f92e8d7c0f348f0ef66657fd0a61edec4add
yjlee9909 <yjlee9909@naver.com>
2022-12-29 20:47:10 +0900

    :art: style : 게시물 스타일 수정

---
+++ src/components/Posts/Comment/PostDate/styledPostDate.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { colors, fonts } from "../../../../GlobalStyle";
+
+export const DateStyle = styled.p`
+  font-weight: 400;
+  font-size: ${fonts.small};
+  line-height: 12px;
+  color: ${colors.color76};
+  margin-left: 54px;
+`;
