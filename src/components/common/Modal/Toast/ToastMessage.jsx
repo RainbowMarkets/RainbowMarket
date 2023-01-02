30b21ab9337e8266a45e21f06572a61665cbc6cb
DADAKIM <d.ya96v@gmail.com>
2023-01-02 19:27:44 +0900

    :art: style: 토스트메세지 UI 구현

--- src/components/common/Modal/Toast/ToastMessage.jsx
+++ src/components/common/Modal/Toast/ToastMessage.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import styled from "styled-components"
+import { colors, fonts } from "../../../../GlobalStyle"
+
+const StyledWrap = styled.section`
+  z-index: 1000;
+  top: 0px;
+  left: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 440px;
+  min-width: 390px;
+  
+  div {
+    width: 252px;
+    flex-direction: column;
+    position: absolute;
+    bottom: 80px;
+    padding: 15px 0;
+    border-radius: 10px;
+    background-color: #fff;
+    border: 2px solid ${colors.colorMain};
+    z-index: 1020;
+    opacity: 1;
+    /* visibility: hidden; */
+    transform: translate(0, 0); /* translate(0, 100%); */
+    transition: all 0.5s;
+    text-align: center;
+  }
+
+  strong{
+    font-weight: bold;
+    font-size: ${fonts.large};
+  }
+`
 
 export default function ToastMessage() {
   return (
-    <div>게시글이 신고되었습니다.</div>
+    <StyledWrap>
+      <h3 className="hidden">신고완료 알림 창</h3>
+      <div>
+        <strong>게시글이 신고되었습니다.</strong>
+      </div>
+    </StyledWrap>
   )
 }
