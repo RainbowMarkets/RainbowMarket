1d2f8ebdcfacd4dc2e15f413f492e659507e36cd
yjlee9909 <yjlee9909@naver.com>
2023-01-26 14:24:51 +0900

    :adhesive_bandage: fix : 게시글 상세 useFetch 사용

--- src/components/Posts/PostDetail.jsx
+++ src/components/Posts/PostDetail.jsx
@@ -6,14 +6,15 @@ import CommentAdd from "./Comment/CommentAdd/CommentAdd";
 import PostContent from "../common/PostContent/PostContent";
 import CommonTopBar from "../TopBar/CommonTopBar/CommonTopBar";
 import Loading from "../common/Loading/Loading";
-
 import { CommentWrapper, PostDiv, PostDetailWrapper } from "./styledPostDetail";
 import CommentModal from "../common/Modal/Modal/CommentModal";
 import { useParams } from "react-router-dom";
 import useUserContext from "../../hooks/useUserContext";
+import useFetch from "../../hooks/useFetch";
 
 const PostDetail = () => {
   const { post_id } = useParams();
+  const { getData } = useFetch();
 
   const [postDetailData, setPostDetailData] = useState({
     comments: [],
@@ -46,21 +47,14 @@ const PostDetail = () => {
   const url = "https://mandarin.api.weniv.co.kr";
   // 게시글 가져오기
   const fetchPostData = async () => {
-    const reqPath = `/post/${post_id}`;
     try {
       setIsPending(true); // 통신 시작
-      const res = await fetch(url + reqPath, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setPostDetailData(data.post);
-          setIsPending(false); // 통신 종료
-        });
+
+      // 게시글 상세 API
+      getData(`/post/${post_id}`).then((data) => {
+        setPostDetailData(data.post);
+        setIsPending(false); // 통신 종료
+      });
     } catch (err) {
       setIsPending(false); // 통신 종료
       console.log("err", err);
